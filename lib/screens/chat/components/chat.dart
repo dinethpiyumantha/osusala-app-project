bd9694d317e75335b6b504f159bbc5a8789875d3
Miru <madhaviimashi@gmail.com>
2021-10-11 12:34:55 +0530

    chat page

---
+++ lib/screens/chat/components/chat.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'body.dart';
+
+class Chat extends StatelessWidget {
+  // const ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
